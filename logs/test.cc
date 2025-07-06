94a18f9476ba21592a6a3d3fdf6c2cb3424bcd9f
myh <zyuhao7@yeah.net>
2025-07-06 22:31:49 +0800

    测试格式化日志消息

--- logs/test.cc
+++ logs/test.cc
@@ -1,17 +1,25 @@
 #include "util.hpp"
 #include "level.hpp"
+#include "formatter.hpp"
 #include <iostream>
+#include <thread>
+#include <string>
 using namespace yhlog::util;
 
 int main() {
-    
-    std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::DEBUG) << std::endl;
-    std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::INFO) << std::endl;
-    std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::WARN) << std::endl;
-    std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::ERROR) << std::endl;
-    std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::FATAL) << std::endl;
-    std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::OFF) << std::endl;
 
+    std::string logger_name = "myh";
+    yhlog::LogMsg msg(logger_name, "main.cc", 27, "Formatted log message test!", yhlog::LogLevel::Value::INFO);
+    yhlog::Formatter fmt("[%d{%Y-%m-%d %H:%M:%S}][%t][%p][%c] %m%n"); 
+    std::string str = fmt.format(msg);                                
+    std::cout << str << std::endl;
+
+    // std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::DEBUG) << std::endl;
+    // std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::INFO) << std::endl;
+    // std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::WARN) << std::endl;
+    // std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::ERROR) << std::endl;
+    // std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::FATAL) << std::endl;
+    // std::cout<< yhlog::LogLevel::toString(yhlog::LogLevel::Value::OFF) << std::endl;
 
 
     // std::string fullPath = "./test_dir1/test_dir2/test_file.txt";
