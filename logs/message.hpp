1485316634b2b36d2a169bd48c572c14ceff1c89
myh <zyuhao7@yeah.net>
2025-07-20 14:14:01 +0800

    日志消息类

--- logs/message.hpp
+++ logs/message.hpp
@@ -11,24 +11,24 @@ namespace yhlog
     {
         using ptr = std::shared_ptr<LogMsg>;
         size_t _line;           // 行号
-        size_t _ctime;           // 时间
+        size_t _ctime;          // 时间
         std::thread::id _tid;   // 线程 id
         std::string _name;      // 日志器名称
         std::string _file;      // 日志文件名
         std::string _payload;   // 日志消息
         LogLevel::Value _level; // 日志等级
-        
-        LogMsg(std::string& name, std::string file, size_t line, std::string&& payload, LogLevel::Value level)
-            :_name(name),
-            _file(file),
-            _payload(payload),
-            _level(level),
-            _line(line),
-            _ctime(util::data::now()),
-            _tid(std::this_thread::get_id())
-            {
-                std::cout << "LogMsg construct success "<< std::endl;
-            }
+
+        LogMsg(std::string &name, std::string file, size_t line, std::string &&payload, LogLevel::Value level)
+            : _name(name),
+              _file(file),
+              _payload(payload),
+              _level(level),
+              _line(line),
+              _ctime(util::data::now()),
+              _tid(std::this_thread::get_id())
+        {
+            std::cout << "LogMsg construct success " << std::endl;
+        }
         ~LogMsg()
         {
             std::cout << "LogMsg destruct success " << std::endl;
